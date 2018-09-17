8f9442d377e82cba0c199e4210c4d3f9e64f0a3b
Adam Hurwitz <adamscotthurwitz@gmail.com>
2018-09-17 10:56:52 -0700

    feature: categorized content trigger

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,26 @@
 const functions = require('firebase-functions');
 
+// The Firebase Admin SDK to access the Firebase Realtime Database.
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+exports.categorizedContent = functions.firestore
+    .document('qa/users/users/{userId}/{categoryType}/{contentTitle}')
+    .onCreate((snap, context) => {
+
+        console.log('Trigger fired!');
+        
+        // Get an object representing the document.
+        // e.g. {'name': 'Marie', 'age': 66}
+        const newValue = snap.data();
+ 
+        // Access a particular field as you would any JS property.
+        //const name = newValue.name;
+ 
+        // perform desired operations ...
+     });
+ 
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
