8bb67fa73caa9d8ad086c270ef25e336d342da3c
Adam Hurwitz <adamscotthurwitz@gmail.com>
2018-09-18 11:23:56 -0700

    feature: get document from Cloud Function

--- functions/index.js
+++ functions/index.js
@@ -4,20 +4,45 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-exports.categorizedContent = functions.firestore
-    .document('qa/users/users/{userId}/{categoryType}/{contentTitle}')
-    .onCreate((snap, context) => {
+const MAIN_FEED_TYPE = "MAIN";
+const SAVED_FEED_TYPE = "SAVED";
+const ARCHIVED_FEED_TYPE = "ARCHIVED";
+const SAVE_USER_ACTION = "SAVE";
+const ARCHIVE_USER_ACTION = "ARCHIVE";
+const SAVED_PATH = "saved"
+const ARCHIVED_PATH = "archived"
 
-        console.log('Trigger fired!');
+exports.updateQualityScore = functions.https.onCall((data, context) => {
+    const environment = data.environment
+    const feedType = data.feedType
+    const action = data.action
+    const contentTitle = data.contentTitle
+    const uid = context.auth.uid;
 
-        // Get an object representing the document.
-        // e.g. {'name': 'Marie', 'age': 66}
-        const newValue = snap.data();
- 
-        // Access a particular field as you would any JS property.
-        //const name = newValue.name;
- 
-        // perform desired operations ...
+    var feedTypePath
+    if (feedType === SAVED_FEED_TYPE) {
+        feedTypePath = SAVED_PATH
+    } else if (feedType === ARCHIVED_FEED_TYPE) {
+        feedTypePath = ARCHIVED_PATH
+    }
 
-        return true;
-    });
+    admin.firestore().collection('qa/content/feeds/main/content/').doc(contentTitle)
+        .get().then(function(doc) {
+            console.log('Trigger fired on content: ' 
+                +  contentTitle +  " | user: " + uid
+                + " | action: " + action + ' | feedType: ' + feedType);
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+            } else {
+                console.log("No such document!");
+            }
+            return {
+                status: 'Get content success.'
+             }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+            return {
+                status: 'Get content error.'
+            }
+        });
+});
