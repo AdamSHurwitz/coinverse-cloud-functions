f4b64393e15913b62623a881aa94dd743f7366fd
AdamHurwitz <adamscotthurwitz@gmail.com>
2018-12-03 21:53:22 -0800

    feature: get audiocasts

--- functions/index.js
+++ functions/index.js
@@ -4,45 +4,13 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-const MAIN_FEED_TYPE = "MAIN";
-const SAVED_FEED_TYPE = "SAVED";
-const ARCHIVED_FEED_TYPE = "ARCHIVED";
-const SAVE_USER_ACTION = "SAVE";
-const ARCHIVE_USER_ACTION = "ARCHIVE";
-const SAVED_PATH = "saved"
-const ARCHIVED_PATH = "archived"
-
-exports.updateQualityScore = functions.https.onCall((data, context) => {
-    const environment = data.environment
-    const feedType = data.feedType
-    const action = data.action
-    const contentTitle = data.contentTitle
-    const uid = context.auth.uid;
-
-    var feedTypePath
-    if (feedType === SAVED_FEED_TYPE) {
-        feedTypePath = SAVED_PATH
-    } else if (feedType === ARCHIVED_FEED_TYPE) {
-        feedTypePath = ARCHIVED_PATH
-    }
-
-    admin.firestore().collection('qa/content/feeds/main/content/').doc(contentTitle)
-        .get().then(function(doc) {
-            console.log('Trigger fired on content: ' 
-                +  contentTitle +  " | user: " + uid
-                + " | action: " + action + ' | feedType: ' + feedType);
-            if (doc.exists) {
-                console.log("Document data:", doc.data());
-            } else {
-                console.log("No such document!");
-            }
-            return {
-                status: 'Get content success.'
-             }
-        }).catch(function(error) {
-            console.log("Error getting document:", error);
-            return {
-                status: 'Get content error.'
-            }
-        });
+// Retrieve the audiocast Cloud Storage path.
+exports.getAudiocast = functions.https.onCall((data, context) => {
+    //TODO: Check if audiocast exists.
+    //TODO: If exists, return file path.
+    //TODO: If doesn't exist, create audiocast, save to Cloud Storage, and return path
+    console.log("Get Audiocast:" + data.id)
+    return {
+        filePath: "cloudStorage/someFilePath",
+      };
 });
