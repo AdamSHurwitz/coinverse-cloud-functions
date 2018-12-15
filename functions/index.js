7d93ba019a099fc1261926d1fbc3c18f3afa08be
AdamHurwitz <adamscotthurwitz@gmail.com>
2018-12-14 22:19:09 -0800

    feature: SSML

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,5 @@
+const Speech = require('ssml-builder');
+const textToSpeech = require('@google-cloud/text-to-speech');
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
@@ -5,14 +7,10 @@ const util = require('util');
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const {Storage} = require('@google-cloud/storage');
-const textToSpeech = require('@google-cloud/text-to-speech');
 
-const client = new textToSpeech.TextToSpeechClient();
 const charLimitError = "Error: 3 INVALID_ARGUMENT: 5000 characters limit exceeded."
 
-admin.initializeApp();
-
-//TODO: Use SSML configuration. 
+admin.initializeApp(); 
 
 // Converts and saves article text into audio.
 exports.getAudiocast = functions.https.onCall((data, context) => {
@@ -25,13 +23,16 @@ exports.getAudiocast = functions.https.onCall((data, context) => {
     storage = new Storage({ projectId: 'carpecoin-media-211903' });
     bucket = storage.bucket('gs://carpecoin-media-211903.appspot.com');    
   }
-  
+
   var fileName = data.id + '.mp3';
   var filePath = "content/feeds/en-audio/" + fileName;
   var tempFile;
   var errorMessage;
 
   return bucket.file(filePath).exists()
+  .catch(err => {
+    console.error("Check if file exists error: " + err);
+  })
   .then(currentData => {
     var exists = currentData[0];
     console.log("Article " + data.id + " exists: " + exists)
@@ -41,15 +42,26 @@ exports.getAudiocast = functions.https.onCall((data, context) => {
         error: errorMessage 
        }
     } else {
-      console.log('Convert Article ' + data.id + ': ' + data.text)
-      return client.synthesizeSpeech({
-        input: { text: data.text },
-        voice: {languageCode: 'en-US', ssmlGender: 'NEUTRAL'},
-        audioConfig: {audioEncoding: 'MP3'},
-      })
+      //TODO: Improve SSML.
+      console.log('Convert Article ' + data.id + ': ' + (new Speech()).say(data.text).ssml())
+      return new textToSpeech.TextToSpeechClient().synthesizeSpeech({
+        input: { ssml: (new Speech).say(data.text).ssml()},
+        voice: {
+          languageCode: 'en-GB',
+          name: 'en-GB-Wavenet-C',
+        },
+        audioConfig: {
+          audioEncoding: 'MP3',
+          pitch: "0.00",
+          speakingRate: "1.00"
+        },
+      });
     }
   }).catch(err => {
-    console.error("Check if file exists error: " + err);
+    if (err.toString() === charLimitError) {
+      errorMessage = "TTS_CHAR_LIMIT_ERROR";
+    }
+    console.error("Synthesize Speech: " + err);
   })
   .then(responses => { 
     tempFile = path.join(os.tmpdir(), fileName);      
@@ -57,16 +69,13 @@ exports.getAudiocast = functions.https.onCall((data, context) => {
     return writeFile(tempFile, responses[0].audioContent, 'binary')
   })
   .catch(err => {
-    if (err.toString() === charLimitError) {
-      errorMessage = "TTS_CHAR_LIMIT_ERROR";
-      console.error("Synthesize Speech: " + err);
-    }
+    console.error("Write Temporary Audio File Error: " + err);
   })
   .then(() => {
     return bucket.upload(tempFile, { destination: filePath })
   })
   .catch(err => {
-    console.error("Write Temporary Audio File Error: " + err);
+    console.error('Upload Audio to GCS Error: ' + err);
   })
   .then(() => {
     return { 
@@ -74,7 +83,4 @@ exports.getAudiocast = functions.https.onCall((data, context) => {
       error: errorMessage 
     }
   })
-  .catch(err => {
-    console.error('Upload Audio to GCS Error: ' + err);
-  });
 });
